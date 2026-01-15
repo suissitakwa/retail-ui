a1630993dce7e828682372dd644007f16cd19a08
suissitakwa <suissitakwa@gmail.com>
2026-01-14 21:21:05 -0800

    update ui to test ci/cd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="text-center">
       <h1 className="mb-4">Welcome to RetailUI</h1>
       <p>Shop the latest fashion products online.</p>
-      <Link to="/shop" className="btn btn-primary mt-3">Start Shopping</Link>
+      <Link to="/shop" className="btn btn-primary mt-3">Start Shopping here</Link>
     </div>
   );
 }
